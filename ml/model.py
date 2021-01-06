52f2ef1f8cb1be9980dabfcd644ee09b21722d77
root <root@vm-lilian.us-central1-a.c.ambient-scope-297614.internal>
2021-01-06 11:28:48 +0000

    add model.py

---
+++ ml/model.py
@@ -0,0 +1,20 @@
+from utils.util import *
+
+def DataManager(d:DataHandler=None, fr: FeatureRecipe=None, fe:FeatureExtractor=None):
+    """
+        Fonction qui lie les 3 premières classes de la pipeline et qui return FeatureExtractor.split(0.1)
+    """
+    data = DataHandler()
+    data.get_process_data()
+    recipe = FeatureRecipe(data.data)
+    recipe.prepare_data()
+    Fextractor = FeatureExtractor(recipe.data)
+    return Fextractor.split(0.1)
+ 
+X_train, X_test, y_train, y_test = DataManager()
+X_train, X_test, y_train, y_test = Fextractor.split(0.1)
+m = ModelBuilder() 
+m.train(X_train, y_train)
+m.print_accuracy(X_test, y_test)
+m.predict_test(X_test)
+m.save_model('/home/lilian/project_cloud_computing/ml/')
